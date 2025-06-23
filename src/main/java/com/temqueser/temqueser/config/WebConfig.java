19fe2ed3d5f3e8428ae5c51b126059c220978902
Thomas Ramos Oliveira <thomas.oliveira@sga.pucminas.br>
2025-06-23 16:56:22 -0300

    Deploy8

--- src/main/java/com/temqueser/temqueser/config/WebConfig.java
+++ src/main/java/com/temqueser/temqueser/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedOrigins("https://front-end-tem-que-ser.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
